fd807af38b4bba7a99a6090d11fd37345a84249b
Mohsen Azimi <me@azimi.me>
2024-08-29 15:18:13 +0200

    feat: add uvcanvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import {
   Calendar,
   CheckCircle,
   Globe,
-  Link,
   MessageCircle,
   RefreshCw,
   ShieldCheck,
 } from 'lucide-react';
+import Link from 'next/link';
 import * as React from 'react';
+import { Novatrix } from 'uvcanvas';
 
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
@@ -16,22 +17,27 @@ export default function HomePage() {
   return (
     <div className='flex flex-col min-h-screen'>
       <main className='flex-1'>
-        <section className='w-full py-12 md:py-24 lg:py-32 xl:py-48'>
-          <div className='container px-4 md:px-6'>
-            <div className='flex flex-col items-center space-y-4 text-center'>
-              <div className='space-y-2'>
-                <h1 className='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none'>
-                  Your Rental, Your Website, Your Way
-                </h1>
-                <p className='mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400'>
-                  Create your own website for your short-term rental in minutes.
-                  Get direct bookings, manage your calendar, and communicate
-                  with guests - all without extra fees.
-                </p>
-              </div>
-              <div className='space-x-4'>
-                <Button>Get Started</Button>
-                <Button variant='outline'>Learn More</Button>
+        <section className='w-full py-12 md:py-24 lg:py-32 xl:py-48 relative'>
+          <div className='absolute inset-0 z-0 opacity-10 overflow-hidden'>
+            <Novatrix />
+          </div>
+          <div className='relative z-10'>
+            <div className='container px-4 md:px-6'>
+              <div className='flex flex-col items-center space-y-4 text-center '>
+                <div className='space-y-2'>
+                  <h1 className='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none'>
+                    Your Rental, Your Website, Your Way
+                  </h1>
+                  <p className='mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400'>
+                    Create your own website for your short-term rental in
+                    minutes. Get direct bookings, manage your calendar, and
+                    communicate with guests - all without extra fees.
+                  </p>
+                </div>
+                <div className='space-x-4'>
+                  <Button>Get Started</Button>
+                  <Button variant='outline'>Learn More</Button>
+                </div>
               </div>
             </div>
           </div>
@@ -301,7 +307,7 @@ export default function HomePage() {
       </main>
       <footer className='flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t'>
         <p className='text-xs text-gray-500 dark:text-gray-400'>
-          © 2024 Acme Inc. All rights reserved.
+          © 2024 Make Your Own Listing Inc. All rights reserved.
         </p>
         <nav className='sm:ml-auto flex gap-4 sm:gap-6'>
           <Link className='text-xs hover:underline underline-offset-4' href='#'>
