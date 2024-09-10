69dbaa13d20ea13d6f508dfca758b98dabd79b74
Mohsen Azimi <me@azimi.me>
2024-09-10 21:09:45 +0200

    feat: playwright

---
+++ e2e/homepage.spec.ts
@@ -0,0 +1,23 @@
+import { expect, test } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Make your own/);
+});
+
+// TODO: We need to handle auth in test
+test.skip('get started link', async ({ page }) => {
+  await page.goto('/');
+
+  // Click the get started link.
+  await page.getByText('Get started').first().click();
+
+  // expect to go to /listing/create
+  await expect(page).toHaveURL(/listing\/create/);
+
+  // expect the form with address field and next button to be rendered
+  await expect(page.getByRole('form')).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Next' })).toBeVisible();
+});
