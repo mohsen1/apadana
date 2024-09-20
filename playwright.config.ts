4fd41fc0fcb7423dec39ad65d6c566a865113057
Mohsen Azimi <me@azimi.me>
2024-09-20 12:13:46 +0200

    ci: retry

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+  timeout: 5_000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html', { open: 'never', outputFolder: '.next/__e2e__reports__' }],
