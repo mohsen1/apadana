5595c955637344cf1888195d8dd315853c968450
Mohsen Azimi <me@azimi.me>
2024-09-20 10:44:54 +0200

    fix: fix

--- src/scripts/build-vercel.sh
+++ src/scripts/build-vercel.sh
@@ -2,7 +2,9 @@
 
 # check if this is a preview deployment
 if [ "$VERCEL_ENV" == "preview" ]; then
-  echo "Production database name: $POSTGRES_DATABASE"
+  # print all environment variables
+  pnpm dlx dotenv -e .env -- echo "Environment variables: $VERCEL_ENV"
+  pnpm dlx dotenv -e .env -- echo "Production database name: $POSTGRES_DATABASE"
   echo "Creating preview database for PR #$VERCEL_GIT_PULL_REQUEST_ID"
   pnpm run manage-db create "preview_db_$VERCEL_GIT_PULL_REQUEST_ID"
   # in case db already exists, purge it
