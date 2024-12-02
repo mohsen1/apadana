ef8c879317845749984e06c64e913f8ff1d11764
Mohsen Azimi <me@azimi.me>
2024-12-02 09:06:04 +0700

    fix test

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,7 +3,3 @@ import '@testing-library/jest-dom';
 // Allow router mocks.
 // eslint-disable-next-line no-undef
 jest.mock('next/router', () => require('next-router-mock'));
-
-jest.mock('@/lib/auth/session', () => ({
-  getSession: () => Promise.resolve({ userId: 'test-user-id' }),
-}));
