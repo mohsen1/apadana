c36c84614c6513f857f30441748c9dc4b8e8e4a2
Mohsen Azimi <me@azimi.me>
2024-12-05 19:08:19 +0700

    exit without db on build

--- src/scripts/build-vercel.sh
+++ src/scripts/build-vercel.sh
@@ -3,6 +3,12 @@
 # Build for production. This script assumes all of the environment variables are set.
 # for build process to work, the following commands must be run
 
+# id DATABASE_URL is not set exit
+if [ -z "$DATABASE_URL" ]; then
+  echo "DATABASE_URL is not set"
+  exit 1
+fi
+
 pnpm prisma generate --no-hints
 pnpm prisma migrate deploy
 pnpm next build
