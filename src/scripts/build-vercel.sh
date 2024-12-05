815190da7b931bb200da0589cd301627da025b44
Mohsen Azimi <me@azimi.me>
2024-12-05 19:10:53 +0700

    more env check

--- src/scripts/build-vercel.sh
+++ src/scripts/build-vercel.sh
@@ -3,11 +3,24 @@
 # Build for production. This script assumes all of the environment variables are set.
 # for build process to work, the following commands must be run
 
-# id DATABASE_URL is not set exit
-if [ -z "$DATABASE_URL" ]; then
-  echo "DATABASE_URL is not set"
-  exit 1
-fi
+# if The following environment variable is not set, exit
+variables=(
+  NEXT_PUBLIC_DOMAIN
+  NEXT_PUBLIC_API_URL
+  GOOGLE_MAPS_API_KEY
+  NEXT_PUBLIC_S3_UPLOAD_BUCKET
+  NEXT_PUBLIC_S3_UPLOAD_REGION
+  RESEND_API_KEY
+  S3_UPLOAD_KEY
+  S3_UPLOAD_SECRET
+)
+
+for variable in "${variables[@]}"; do
+  if [ -z "${!variable}" ]; then
+    echo "$variable is not set"
+    exit 1
+  fi
+done
 
 pnpm prisma generate --no-hints
 pnpm prisma migrate deploy
