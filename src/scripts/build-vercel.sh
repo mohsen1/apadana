f479680692b6d7e5a1f6ae3fe418ed0a37be50e9
Mohsen Azimi <me@azimi.me>
2024-12-05 19:14:56 +0700

    Set NEXT_PUBLIC_DOMAIN

--- src/scripts/build-vercel.sh
+++ src/scripts/build-vercel.sh
@@ -5,7 +5,6 @@
 
 # if The following environment variable is not set, exit
 variables=(
-  NEXT_PUBLIC_DOMAIN
   NEXT_PUBLIC_API_URL
   GOOGLE_MAPS_API_KEY
   NEXT_PUBLIC_S3_UPLOAD_BUCKET
@@ -14,6 +13,9 @@ variables=(
   S3_UPLOAD_KEY
   S3_UPLOAD_SECRET
 )
+
+# set the domain to the vercel url. this value is dynamic and changes on each deploy depending on the environment
+export NEXT_PUBLIC_DOMAIN=$VERCEL_URL
 
 for variable in "${variables[@]}"; do
   if [ -z "${!variable}" ]; then
