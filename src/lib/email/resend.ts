9359f2c78769fc5aecc2c37b128d1042d81a6516
Mohsen Azimi <me@azimi.me>
2024-12-15 14:07:38 +0700

    fix deployments

--- src/lib/email/resend.ts
+++ src/lib/email/resend.ts
@@ -3,8 +3,9 @@ import { Resend } from 'resend';
 import { LocalResend } from '@/lib/email/local-resend';
 
 export const isUsingLocalResend =
-  process.env.NODE_ENV === 'development' ||
-  process.env.NEXT_PUBLIC_TEST_ENV === 'e2e';
+  (process.env.NODE_ENV === 'development' ||
+    process.env.NEXT_PUBLIC_TEST_ENV === 'e2e') &&
+  !process.env.VERCEL_URL?.includes('apadana.app');
 
 const resend = isUsingLocalResend
   ? new LocalResend()
