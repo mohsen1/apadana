800f0119fe48140122e6ce00a1c844eb27b12f15
Mohsen Azimi <me@azimi.me>
2024-12-15 14:34:04 +0700

    Do not require LocalResend in production

--- src/lib/email/resend.ts
+++ src/lib/email/resend.ts
@@ -1,6 +1,15 @@
 import { Resend } from 'resend';
 
-import { LocalResend } from '@/lib/email/local-resend';
+/**
+ * Here we use a dynamic import to avoid importing the local-resend module in the main entry file
+ * when building for production.
+ */
+function getLocalResend() {
+  type ResendModule = typeof import('@/lib/email/local-resend');
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const { LocalResend } = require('@/lib/email/local-resend') as ResendModule;
+  return new LocalResend();
+}
 
 export const isUsingLocalResend =
   (process.env.NODE_ENV === 'development' ||
@@ -8,7 +17,7 @@ export const isUsingLocalResend =
   !process.env.VERCEL_URL?.includes('apadana.app');
 
 const resend = isUsingLocalResend
-  ? new LocalResend()
+  ? getLocalResend()
   : new Resend(process.env.RESEND_API_KEY);
 
 export default resend;
