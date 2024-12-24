3c32746e128caaf874e35d466bb0a0a0fadff046
Mohsen Azimi <me@azimi.me>
2024-12-24 10:46:09 +0700

    Improve tailwind config

--- config/tailwind.config.ts
+++ config/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
 
 const content = [
   'src/**/*.{js,ts,jsx,tsx,mdx}',
@@ -16,22 +17,27 @@ const config: Config = {
         primary: {
           DEFAULT: 'var(--primary)',
           foreground: 'var(--primary-foreground)',
+          hover: 'color-mix(in srgb, var(--primary) 85%, black)',
         },
         secondary: {
           DEFAULT: 'var(--secondary)',
           foreground: 'var(--secondary-foreground)',
+          hover: 'color-mix(in srgb, var(--secondary) 85%, black)',
         },
         muted: {
           DEFAULT: 'var(--muted)',
           foreground: 'var(--muted-foreground)',
+          hover: 'color-mix(in srgb, var(--muted) 85%, black)',
         },
         accent: {
           DEFAULT: 'var(--accent)',
           foreground: 'var(--accent-foreground)',
+          hover: 'color-mix(in srgb, var(--accent) 85%, black)',
         },
         destructive: {
           DEFAULT: 'var(--destructive)',
           foreground: 'var(--destructive-foreground)',
+          hover: 'color-mix(in srgb, var(--destructive) 85%, black)',
         },
 
         /* System Colors */
@@ -55,29 +61,44 @@ const config: Config = {
         success: {
           DEFAULT: 'var(--success)',
           foreground: 'var(--success-foreground)',
+          hover: 'color-mix(in srgb, var(--success) 85%, black)',
         },
         warning: {
           DEFAULT: 'var(--warning)',
           foreground: 'var(--warning-foreground)',
+          hover: 'color-mix(in srgb, var(--warning) 85%, black)',
         },
 
         /* Additional Base Colors */
         slate: {
           DEFAULT: 'var(--slate)',
           foreground: 'var(--slate-foreground)',
+          hover: 'color-mix(in srgb, var(--slate) 85%, black)',
         },
         stone: {
           DEFAULT: 'var(--stone)',
           foreground: 'var(--stone-foreground)',
+          hover: 'color-mix(in srgb, var(--stone) 85%, black)',
         },
         zinc: {
           DEFAULT: 'var(--zinc)',
           foreground: 'var(--zinc-foreground)',
+          hover: 'color-mix(in srgb, var(--zinc) 85%, black)',
         },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addVariant }) => {
+      addVariant('state-active', '&[data-state="active"]');
+      addVariant('state-open', '&[data-state="open"]');
+      addVariant('state-closed', '&[data-state="closed"]');
+      addVariant('state-on', '&[data-state="on"]');
+      addVariant('state-off', '&[data-state="off"]');
+      addVariant('state-checked', '&[data-state="checked"]');
+      addVariant('state-unchecked', '&[data-state="unchecked"]');
+    }),
+  ],
 };
 
 export default config;
