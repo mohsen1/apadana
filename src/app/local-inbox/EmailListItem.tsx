fafb78bac8bdd001b96fafbfa46bfc16de264220
Mohsen Azimi <me@azimi.me>
2024-12-27 09:57:32 +0700

    reset password polish

--- src/app/local-inbox/EmailListItem.tsx
+++ src/app/local-inbox/EmailListItem.tsx
@@ -11,7 +11,7 @@ export function EmailListItem({ email, isSelected, onClick }: EmailListItemProps
   return (
     <li
       className={`border-border hover:bg-accent/50 cursor-pointer border-b p-4 transition-colors ${
-        isSelected ? 'bg-accent border-l-primary border-l-4' : ''
+        isSelected ? 'border-l-primary border-l-4 bg-zinc-200' : ''
       }`}
       onClick={onClick}
     >
