e6f1fb84d74fd8567a2d68869f625566da831c5e
Mohsen Azimi <me@azimi.me>
2024-12-28 19:17:48 +0700

    Always run in parallel

--- config/playwright.storybook.config.ts
+++ config/playwright.storybook.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: undefined, // always run in parallel
 
   reporter: [['html', { outputFolder: path.join(process.cwd(), './storybook-e2e-html-report') }]],
 
