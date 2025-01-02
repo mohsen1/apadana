33a729da98495acde6c3c55798d6715c2bc8c22d
Mohsen Azimi <me@azimi.me>
2025-01-02 10:09:43 +0700

    Add Apadna Automation Github App

--- src/e2e/auth/email-verification.spec.ts
+++ src/e2e/auth/email-verification.spec.ts
@@ -16,10 +16,7 @@ test.describe('Email Verification Process', () => {
   test('resend verification email appears in inbox', async ({ page, data, baseURL }) => {
     expect(baseURL).toBeDefined();
     // Skip this test when testing against production since we do not have local inbox in prod
-    test.skip(
-      !!baseURL?.toLowerCase().includes('apadana.app'),
-      'Skipping test for production',
-    );
+    test.skip(!!baseURL?.toLowerCase().includes('apadana.app'), 'Skipping test for production');
 
     // 1. Create new account
     await data.createUser(testUser.email, testUser.password);
