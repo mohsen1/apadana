c127035ec959a27d4a00f802b48137ef3f5f37c7
Mohsen Azimi <me@azimi.me>
2025-01-05 11:23:35 +0700

    feat: create new VPC with public, private, and isolated subnets

--- config/aws-setup/stacks/shared-network-stack.ts
+++ config/aws-setup/stacks/shared-network-stack.ts
@@ -8,15 +8,33 @@ export class SharedNetworkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Import existing shared VPC
-    this.vpc = ec2.Vpc.fromLookup(this, 'SharedVpc', {
-      tags: {
-        Name: 'apadana-shared-vpc',
-      },
+    // Create a new VPC with public and private subnets
+    this.vpc = new ec2.Vpc(this, 'SharedVpc', {
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+      maxAzs: 2,
+      natGateways: 1,
+      subnetConfiguration: [
+        {
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+        {
+          name: 'Private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 24,
+        },
+        {
+          name: 'Isolated',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24,
+        },
+      ],
     });
 
     // Tag all resources
     cdk.Tags.of(this).add('Project', 'Apadana');
+    cdk.Tags.of(this.vpc).add('Name', 'apadana-shared-vpc');
 
     // Output VPC ID for cross-stack references
     new cdk.CfnOutput(this, 'SharedVpcId', {
