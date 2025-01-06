681c65ab04741e4af0b56ad0dc244a723db3e488
Mohsen Azimi <me@azimi.me>
2025-01-06 22:30:41 +0700

    feat(aws): add VPC lookup to SharedNetworkStack
    - Add lookup for existing VPC before creating new one
    
    - Update VPC type to IVpc for better compatibility
    
    - Add error handling with assertError

--- src/aws-setup/stacks/shared-network-stack.ts
+++ src/aws-setup/stacks/shared-network-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { aws_ec2 as ec2 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
+import { assertError } from '@/utils';
 import { createLogger } from '@/utils/logger';
 
 const logger = createLogger(__filename);
@@ -11,18 +12,28 @@ interface SharedNetworkStackProps extends cdk.StackProps {
 }
 
 export class SharedNetworkStack extends cdk.Stack {
-  public readonly vpc: ec2.Vpc;
+  public readonly vpc: ec2.IVpc;
 
   constructor(scope: Construct, id: string, props: SharedNetworkStackProps) {
     super(scope, id, props);
 
-    logger.info(`Creating network stack for environment: ${props.environment}`);
+    logger.info(`Creating/importing network stack for environment: ${props.environment}`);
 
-    this.vpc = new ec2.Vpc(this, 'ApadanaVpc', {
-      maxAzs: 2,
-      natGateways: 1,
-      vpcName: `ap-vpc-${props.environment}`,
-    });
-    logger.debug('Created VPC');
+    // Try to look up existing VPC first
+    try {
+      this.vpc = ec2.Vpc.fromLookup(this, 'ExistingVpc', {
+        vpcName: `ap-vpc-${props.environment}`,
+      });
+      logger.debug('Found existing VPC');
+    } catch (error) {
+      assertError(error);
+      logger.debug('No existing VPC found, creating new one');
+      this.vpc = new ec2.Vpc(this, 'ApadanaVpc', {
+        maxAzs: 2,
+        natGateways: 1,
+        vpcName: `ap-vpc-${props.environment}`,
+      });
+      logger.debug('Created new VPC');
+    }
   }
 }
