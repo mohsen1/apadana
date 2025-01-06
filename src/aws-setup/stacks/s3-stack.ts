ac95c46b39d3dd3f3e1abd10f856edd520a19a4b
Mohsen Azimi <me@azimi.me>
2025-01-06 16:26:18 +0700

    fix: use proper HttpMethods enum for OPTIONS in CORS config

--- src/aws-setup/stacks/s3-stack.ts
+++ src/aws-setup/stacks/s3-stack.ts
@@ -1,6 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
 import { aws_s3 as s3 } from 'aws-cdk-lib';
-import { aws_iam as iam } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 import { createLogger } from '@/utils/logger';
@@ -43,6 +42,7 @@ export class S3Stack extends cdk.Stack {
             s3.HttpMethods.POST,
             s3.HttpMethods.DELETE,
             s3.HttpMethods.HEAD,
+            'OPTIONS' as s3.HttpMethods,
           ],
           allowedOrigins: [
             'https://*.apadana.local',
@@ -62,21 +62,6 @@ export class S3Stack extends cdk.Stack {
       ],
     });
     logger.debug('Created S3 bucket');
-
-    // Add bucket policy to allow public read access
-    const bucketPolicy = new s3.BucketPolicy(this, 'BucketPolicy', {
-      bucket: bucket,
-    });
-
-    bucketPolicy.document.addStatements(
-      new iam.PolicyStatement({
-        actions: ['s3:GetObject'],
-        effect: iam.Effect.ALLOW,
-        principals: [new iam.AnyPrincipal()],
-        resources: [bucket.arnForObjects('*')],
-      }),
-    );
-    logger.debug('Added public read access policy to bucket');
 
     bucket.addLifecycleRule({
       abortIncompleteMultipartUploadAfter: cdk.Duration.days(7),
