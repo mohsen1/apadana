ddc4d98f0edf8078a847a1780ae8ac2079aea194
Mohsen Azimi <me@azimi.me>
2025-01-07 21:02:37 +0700

    chore: remove console.log from playwright config

--- src/e2e/playwright.config.ts
+++ src/e2e/playwright.config.ts
@@ -11,9 +11,6 @@ import { z } from 'zod';
 
 dotenv.config();
 
-// eslint-disable-next-line no-console
-console.log('BASE_URL', process.env.BASE_URL);
-
 // Validate environment variables. Run only once to avoid running the validation
 // on every test run.
 _.memoize(() => {
