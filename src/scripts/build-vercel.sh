e29a3ee156f97be8f5a0db9c77cdd12a269119da
Mohsen Azimi <me@azimi.me>
2025-01-07 21:49:09 +0700

    fix: do AWS infra setup only if there is a change

--- src/scripts/build-vercel.sh
+++ src/scripts/build-vercel.sh
@@ -21,45 +21,45 @@ if echo "$diff_output" | grep -q "There were no differences" && ! echo "$diff_ou
 else
   echo "Changes detected. Proceeding with deployment..."
   pnpm cdk:deploy --all --require-approval never --concurrency 10
+
+  # Check deployment time
+  end_time=$(date +%s)
+  elapsed=$((end_time - start_time))
+  if [ $elapsed -ge 2400 ]; then
+    echo "Warning: AWS deployment took more than 40 minutes. This is likely because a lot of resources are being deployed."
+    echo "Vercel build times are capped at 45 minutes. For faster iterations, consider running locally with:"
+    echo "AWS_DEPLOYMENT_STACK_ENV=$AWS_DEPLOYMENT_STACK_ENV pnpm cdk:deploy --all --concurrency 10"
+    exit 124
+  fi
+
+  # Install Vercel CLI
+  echo "Installing Vercel CLI..."
+  npm install --global --silent vercel@latest
+
+  # Get AWS environment variables and set them in Vercel
+  echo "Setting AWS environment variables in Vercel..."
+  pnpm -- aws:env >.env.production 2>&1 || exit 1
+
+  cat /tmp/deployment-values.env | while IFS='=' read -r key value; do
+    # Write the environment variable to a temporary file.
+    # This is done because of how Vercel CLI handles environment variables.
+    filename="/tmp/__TEMP_ENV_VAR__$key.env"
+    echo "$value" >$filename
+    cat $filename | vercel env add "$key" "$VERCEL_ENV" --force --token "$VERCEL_TOKEN"
+    rm -f $filename
+    # Export the environment variable to the current shell session for the build script to use.
+    export $key=$value
+
+    # Wait for resources to be ready
+    echo "Waiting for AWS resources to be ready..."
+    pnpm prisma:generate
+    pnpm cdk:wait
+
+    # Deploy Prisma migrations
+    echo "Deploying database migrations..."
+    pnpm prisma:migrate
+  done
 fi
-
-# Check deployment time
-end_time=$(date +%s)
-elapsed=$((end_time - start_time))
-if [ $elapsed -ge 2400 ]; then
-  echo "Warning: AWS deployment took more than 40 minutes. This is likely because a lot of resources are being deployed."
-  echo "Vercel build times are capped at 45 minutes. For faster iterations, consider running locally with:"
-  echo "AWS_DEPLOYMENT_STACK_ENV=$AWS_DEPLOYMENT_STACK_ENV pnpm cdk:deploy --all --concurrency 10"
-  exit 124
-fi
-
-# Install Vercel CLI
-echo "Installing Vercel CLI..."
-npm install --global --silent vercel@latest
-
-# Get AWS environment variables and set them in Vercel
-echo "Setting AWS environment variables in Vercel..."
-pnpm -- aws:env >.env.production 2>&1 || exit 1
-
-cat /tmp/deployment-values.env | while IFS='=' read -r key value; do
-  # Write the environment variable to a temporary file.
-  # This is done because of how Vercel CLI handles environment variables.
-  filename="/tmp/__TEMP_ENV_VAR__$key.env"
-  echo "$value" >$filename
-  cat $filename | vercel env add "$key" "$VERCEL_ENV" --force --token "$VERCEL_TOKEN"
-  rm -f $filename
-  # Export the environment variable to the current shell session for the build script to use.
-  export $key=$value
-done
-
-# Wait for resources to be ready
-echo "Waiting for AWS resources to be ready..."
-pnpm prisma:generate
-pnpm cdk:wait
-
-# Deploy Prisma migrations
-echo "Deploying database migrations..."
-pnpm prisma:migrate
 
 # In preview run seed
 if [ "$VERCEL_ENV" == "preview" ]; then
