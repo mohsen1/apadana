29482a3c0028185617b40e109943405cbdf2bd7a
Mohsen Azimi <me@azimi.me>
2025-01-08 20:54:34 +0700

    playwright: always in parallel

--- src/e2e/playwright.config.ts
+++ src/e2e/playwright.config.ts
@@ -60,8 +60,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Always run tests in parallel */
+  workers: undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     [
