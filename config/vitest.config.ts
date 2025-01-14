1d39f5ed4a2122f808088bee19d2ebe043902836
Mohsen Azimi <me@azimi.me>
2025-01-15 01:27:18 +0700

    fix prisma type generator

--- config/vitest.config.ts
+++ config/vitest.config.ts
@@ -5,7 +5,8 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  plugins: [react(), tsconfigPaths() as any],
   test: {
     silent: Boolean(process.env.CI),
     globals: true,
